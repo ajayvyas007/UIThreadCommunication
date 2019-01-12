4189bc5a1866a9aedc4bb7f1000f077d71365d0d
ajay <ajayv@selectsourceintl.com>
2019-01-13 00:33:26 +0530

    Show With Handler

--- src/main/java/com/example/ajay/uicommunicationthread/MainActivity.kt
+++ src/main/java/com/example/ajay/uicommunicationthread/MainActivity.kt
@@ -2,20 +2,25 @@ package com.example.ajay.uicommunicationthread
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var th:Thread;
+    lateinit var handler: Handler;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        handler = Handler()
+
         th = Thread(Runnable {
             for (x in 1..10){
                 showData(x)
+                showProgress(x)
                 Thread.sleep(1000)
             }
         })
@@ -26,5 +31,11 @@ class MainActivity : AppCompatActivity() {
 
     fun showData(a:Int){
         runOnUiThread { textView.setText(Integer.toString(a)) }
+    }
+
+    fun showProgress(a:Int){
+        handler.postDelayed(Runnable(){
+            run { textView2.setText(Integer.toString(a)) }
+        },100);
     }
 }
